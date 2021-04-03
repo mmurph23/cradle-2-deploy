08b9e68d36ed835d703efbf3300b5eb5f864d69a
craigpage <craigpage96@yahoo.com>
2021-04-03 09:04:37 -0400

    wip
    wip

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bruh
+          Bro1234
         </p>
       </header>
     </div>
